051bd0476c728cbc14c9b30ec0d814bbdff519a1
Username <codrinaignat@hotmail.com>
2023-11-22 22:18:20 +0200

    Modified function to save values different than 0

--- CheckAmplZeroCrossfunction.py
+++ CheckAmplZeroCrossfunction.py
@@ -1,83 +1,66 @@
-def CheckAmplZeroCross(signalc):  
+#Original code from Bogdan:
+# def CheckAmplZeroCross(signalc):  
  
-  myBoolean = true
-  Vector = [] 
-  maxim = TPT.FloatX()
-  minim = TPT.FloatX()
+#   myBoolean = true
+#   Vector = [] 
+#   maxim = TPT.FloatX()
+#   minim = TPT.FloatX()
+#   maxim=0
+#   minim=0
+  
+# #  inputsignalc_deriv_previos2 = TPT.Double()      
+# #  inputsignalc_deriv2         = TPT.Double()
+#   if signalc(@)>=0:
+#     myBoolean=true
+#   else:
+#     myBoolean=false
+# #  during TPT.regexp([t>@]):
+#   if myBoolean==true:
+#       if signalc>maxim:
+#         maxim=signalc
+#       if signalc==0:
+#         myBoolean=false
+#         Vector.append(maxim)
+#         maxim=0
+#   else:
+#       if signalc(t) < minim:
+#         minim=signalc(t)
+#       if signalc == 0:
+#         myBoolean=true
+#         Vector.append(minim)
+#         minim=0
+#   return Vector     
+
+
+def CheckAmplZeroCross(signalc):  
+  myBoolean = True
+  myArray = []
   maxim=0
   minim=0
   
-#  inputsignalc_deriv_previos2 = TPT.Double()      
-#  inputsignalc_deriv2         = TPT.Double()
-  if signalc(@)>=0:
-    myBoolean=true
-  else:
-    myBoolean=false
+  # if signalc(@)>=0:
+  #   myBoolean=True
+  # else:
+  #   myBoolean=False
 #  during TPT.regexp([t>@]):
-  if myBoolean==true:
-      if signalc>maxim:
-        maxim=signalc
-      if signalc==0:
-        myBoolean=false
-        Vector.append(maxim)
-        maxim=0
-  else:
-      if signalc(t) < minim:
-        minim=signalc(t)
-      if signalc == 0:
-        myBoolean=true
-        Vector.append(minim)
-        minim=0
-  return Vector     
+  for element in range(0, len(signalc)):
+    #if myBoolean==True:
+        if signalc[element]>maxim:
+          maxim=signalc[element]
+          myArray.append(maxim)
+        if signalc[element]==0:
+          #myBoolean=False
+          #Vector.append(maxim)
+          maxim=0
+    #elif myBoolean==False:
+        if signalc[element] < minim:
+          minim=signalc[element]
+          myArray.append(minim)
+        if signalc == 0:
+          #myBoolean=True
+          
+          minim=0
+  return myArray     
 
-
-TRY:
-
-import numpy as np
-from scipy import fft
-from matplotlib import pyplot as plt
-
-sr = 200
-time = np.arange(0, 60, 1/sr)
-
-amp = 2
-F = 0.25
-omega = 2 * np.pi * F
-
-signal1 = amp*np.sin(omega * time + np.pi)
-signal2 = amp*np.sin(omega * time + np.pi/3)
-
-mu, sigma = 0.2, 0.1 # mean and standard deviation
-noise1 = np.random.normal(mu, sigma, len(signal1))
-noise2 = np.random.normal(mu, sigma, len(signal2))
-
-
-noisy_signal1 = signal1 + noise1
-noisy_signal2 = signal2 + noise2
-
-# Get frequency in samples
-fft1 = fft.fft(noisy_signal1)
-fft2 = fft.fft(noisy_signal2)
-max_f = np.argmax(np.abs(fft1))
-f = len(noisy_signal1) / max_f
-
-# Perform correlation to determine best fit for phase difference (in samples)
-corr = np.correlate(noisy_signal1, noisy_signal2, 'full')
-delta_p = int(round(np.argmax(corr) % f))
-
-# Chop phase difference (maximum 1/2 cycle)
-fi = int(round(f,0))
-if delta_p < fi/ 2:
-    noisy_signal1 = noisy_signal1[delta_p:]
-    noisy_signal2 = noisy_signal2[:-delta_p]
-    time = time[delta_p:]
-else:
-    noisy_signal1 = noisy_signal1[:delta_p - fi]
-    noisy_signal2 = noisy_signal2[fi - delta_p:]
-    time = time[:delta_p - fi]
-
-plt.figure()
-plt.plot(time, noisy_signal1)
-plt.plot(time, noisy_signal2)
-plt.show() 
-
+mySignal = [1, 2, 3, 2, 1, 0, -1, -2, -3, -2, -1, 0, 1, 2, 3, 2, 1, 0, -1, -2, -3, -2, -1, 0, 1, 2]
+print(CheckAmplZeroCross(mySignal))
