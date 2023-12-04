2ffda636f14e125c6dc907eb1c0518d268236e9e
Codrina Lisaru <94629883+codrinalisaru@users.noreply.github.com>
2023-12-04 08:49:23 +0200

    Update CheckAmplZeroCrossfunction.py

--- CheckAmplZeroCrossfunction.py
+++ CheckAmplZeroCrossfunction.py
@@ -71,3 +71,43 @@ print(CheckAmplZeroCross(mySignal))
 #     print(mySignal[i])
 
 
+#Best version
+
+def CheckAmplZeroCross(signalc):  
+
+  myArray = []
+  maxim=0
+  minim=0
+  myBoolean = True
+
+  for element in range(0, len(signalc)):
+      if myBoolean == True:
+            if signalc[element]>maxim:
+              maxim=signalc[element]
+    
+            if signalc[element]==0:
+              myBoolean = False
+              myArray +=[maxim]
+              maxim=0
+      else:
+            if signalc[element] < minim:
+                minim=signalc[element]
+
+            if signalc[element] == 0:
+                myBoolean = True
+                myArray += [minim]
+                minim=0
+
+  return myArray     
+
+mySignal = [1, 2, 5, 2, 1, 0, -1, -2, -5, -2, -1, 0, 1, 2, 3, 2, 1, 0, -1, -2, -3, -2, -1, 0, 1, 2]
+print(CheckAmplZeroCross(mySignal))
+
+mysecondsignal = [1, 2, 5, 2, 1, 0, -1, -2, -5, -2, -1, 0, 1, 2, 3, 2, 1, 0, -1, -2, -3, -2, -1, 0, 1, 2]
+print(CheckAmplZeroCross(mysecondsignal))
+for i in range(0, len(mySignal)):
+    if (mySignal[i] == (mysecondsignal[i])):
+        print("Samples equal")
+    else:
+        print("Different samples")
+
