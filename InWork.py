d9ed89561c9dd073ab8f767f079749c47b8db0f1
Codrina Lisaru <94629883+codrinalisaru@users.noreply.github.com>
2023-12-06 09:03:49 +0200

    Update InWork.py

--- InWork.py
+++ InWork.py
@@ -1,30 +1,3 @@
-def CheckAmplZeroCross(signalc):  
-
-  myArray = []
-  maxim=0
-  minim=0
-  isPositiveCycle = True
-
-  for i in range(0, len(signalc)):
-      if isPositiveCycle == True:
-            if signalc[i]>maxim:
-              maxim=signalc[i]
-    
-            if signalc[i]==0:
-              isPositiveCycle = False
-              myArray +=[maxim]
-              maxim=0
-      else:
-            if signalc[i] < minim:
-                minim=signalc[i]
-
-            if signalc[i] == 0:
-                isPositiveCycle = True
-                myArray += [minim]
-                minim=0
-
-  return myArray     
-
 def MinMax (signalc):
     myDerivative = []
     minMax = []
@@ -39,12 +12,5 @@ def MinMax (signalc):
     return 
 
 mySignal = [1, 2, 5, 2, 1, 0, -1, -2, -5, -2, -1, 0, 1, 2, 3, 2, 1, 0, -1, -2, -3, -2, -1, 0, 1, 2]
-print(CheckAmplZeroCross(mySignal))
 
 mysecondsignal = [1, 2, 5, 2, 1, 0, -1, -2, -5, -2, -1, 0, 1, 2, 3, 2, 1, 0, -1, -2, -3, -2, -1, 0, 1, 2]
-print(CheckAmplZeroCross(mysecondsignal))
-for i in range(0, len(mySignal)):
-    if (mySignal[i] == (mysecondsignal[i])):
-        print("Samples equal")
-    else:
-        print("Different samples")
